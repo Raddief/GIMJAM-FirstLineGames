6ad8ad25b4c3b6a7028e4b1cdb398b0ab83e53e9
Salim <salimalhaddad1211@gmail.com>
2026-01-26 16:05:06 +0700

    day modifier

--- scripts/managers/alien_manager.gd
+++ scripts/managers/alien_manager.gd
@@ -3,6 +3,7 @@ class_name AlienManager
 
 @export var alien_scenes : Array[PackedScene]
 @export var grid_manager : GridManager
+@export var day_manager : DayManager
 
 	
 func spawn_alien(scene: PackedScene):
@@ -24,9 +25,17 @@ func _find_empty_cell():
 	return null
 
 func on_turn_passed():
-	for alien in get_children():
-		if alien.has_method("on_turn_passed"):
-			alien.on_turn_passed()
+	var production_triggers := 1
+
+	for modifier in day_manager.active_modifiers:
+		if modifier.has_method("on_turn_passed"):
+			modifier.on_turn_passed()
+		if modifier.has_method("extra_production_trigger"):
+			production_triggers += modifier.extra_production_trigger()
+	for i in range(production_triggers):
+		for alien in get_children():
+			if alien.has_method("on_turn_passed"):
+				alien.on_turn_passed()
 
 func on_day_ended():
 	for alien in get_children():
