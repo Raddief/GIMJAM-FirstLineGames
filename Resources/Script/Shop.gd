95bf19a5aeb56110927c08b7201788ca196207e5
Jack-of-all-trades-04 <novanagung08@gmail.com>
2026-01-26 15:48:19 +0700

    Add desc, origin, etc

--- Resources/Script/Shop.gd
+++ Resources/Script/Shop.gd
@@ -8,22 +8,34 @@ var Mainmenu = preload("res://Resources/Scene/Button.tscn") #Masih Placeholder i
 @onready var anim = $Animation
 
 #Variable Status
-var day = 1
+var day = 6
 @export var Database:GDScript
 
-func showDesc():
+func showDesc(types:int, object:String):
 	$BottomPanel.visible = true
+	if types == 0 :
+		var Data:Dictionary = Database.new().Alien
+		$BottomPanel/Description/Icon.set_text(object)
+		$BottomPanel/Description/Contain1.set_text(str(Data.get(object)[3]))
+		$BottomPanel/Description/Contain2.set_text(Data.get(object)[0])
+		$BottomPanel/Description/Contain3.set_text(Data.get(object)[1])
 
 func addSlot(types:int):
-	match day:
-		1:
-			for i in 4+day:
-				var slot = slots.instantiate()
-				slot.slot = true
-				slot.type = types
-				slot.root = self
-				slot.index = i
-				$SidePanel/Scroll/Scroll/Stock.add_child(slot)
+	var Data:Dictionary
+	var keys
+	if types == 0:
+		Data= Database.new().Alien
+		keys = Data.keys()
+	else :
+		Data = Database.new().Item
+		keys = Data.keys()
+	for i in 4+day:
+		var slot = slots.instantiate()
+		slot.slot = true
+		slot.type = types
+		slot.root = self
+		slot.object = keys[i]
+		$SidePanel/Scroll/Scroll/Stock.add_child(slot)
 
 func _on_open_close_pressed() -> void:
 	if $BottomPanel.visible == false && $SidePanel.get_position().x != 1111:
@@ -32,7 +44,8 @@ func _on_open_close_pressed() -> void:
 		anim.play("CloseShop")
 
 func _on_shop_pressed() -> void:
-	if $SidePanel/Menu/MenuList/Shop.get_text() == "Shop" :
+	var text = $SidePanel/Menu/MenuList/Shop.get_text()
+	if text == "Shop" :
 		$SidePanel/Menu/MenuList/Shop.set_text("Aliens")
 		$SidePanel/Menu/MenuList/Shop.set_button_icon(load("res://Resources/Asset/UI/alien-stare.png"))
 		$SidePanel/Menu/MenuList/UseItem.set_text("Items")
@@ -40,7 +53,7 @@ func _on_shop_pressed() -> void:
 		$SidePanel/Menu/MenuList/Journal.set_button_icon(null)
 		$SidePanel/Menu/MenuList/Setting.visible = false
 		$SidePanel/Menu/MenuList/Forfeit.visible = false
-	elif $SidePanel/Menu/MenuList/Shop.get_text() == "Aliens" :
+	elif text == "Aliens" :
 		addSlot(0)
 		$SidePanel/Menu.visible = false
 		$SidePanel/Scroll.visible = true
@@ -49,7 +62,8 @@ func _on_use_item_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_journal_pressed() -> void:
-	if $SidePanel/Menu/MenuList/Journal.get_text() == "Back":
+	var text = $SidePanel/Menu/MenuList/Journal.get_text()
+	if text == "Back":
 		$SidePanel/Menu/MenuList/Shop.set_text("Shop")
 		$SidePanel/Menu/MenuList/Shop.set_button_icon(load("res://Resources/Asset/UI/shopping-cart.png"))
 		$SidePanel/Menu/MenuList/UseItem.set_text("Use Item")
@@ -57,6 +71,8 @@ func _on_journal_pressed() -> void:
 		$SidePanel/Menu/MenuList/Journal.set_button_icon(load("res://Resources/Asset/UI/secret-book.png"))
 		$SidePanel/Menu/MenuList/Setting.visible = true
 		$SidePanel/Menu/MenuList/Forfeit.visible = true
+	elif text == "Journal" :
+		pass
 
 func _on_animation_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "CloseShop" :
